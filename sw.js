1f23d6c0c98fdc93adf533cccd1a920a8b9e0858
research-river <research.river1@gmail.com>
2026-05-09 17:01:15 +0900

    toilet_icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'suijin-map-v4';
+const CACHE = 'suijin-map-v6';
 
 const APP_SHELL = [
   './suijin_map.html',
@@ -8,6 +8,7 @@ const APP_SHELL = [
   './maplibre-gl.js',
   './maplibre-gl.css',
   './togeojson.min.js',
+  './toilet.png',
   './arakawa_suijin_marker.kml',
   './トイレmap 荒川CR0508.kml',
   './d_west.gpx',
